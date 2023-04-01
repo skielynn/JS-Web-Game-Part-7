e31d0dc240b5efe4623bb514ef26a64ed66b17ce
Skie <skielynn.1105945@gmail.com>
2023-03-31 19:01:27 -0700

    complete

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -57,3 +57,10 @@ function newNonPlayableCharacter(x, y) {
         stop: stop
     }
 }
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
